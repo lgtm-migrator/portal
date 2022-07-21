300cfba1de12816a5ee267cfd5738d69ce26f4fb
Kevin Utoft <kevin.utoft@gmail.com>
2022-07-21 14:56:39 -0500

    remove old verify logic and simplify flow (#422)
    * remove old verify logic and simplify flow
    
    * update verify screen copy

--- packages/frontend/src/screens/Onboarding/Validate.jsx
+++ packages/frontend/src/screens/Onboarding/Validate.jsx
@@ -1,140 +1,50 @@
 import { useCallback, useEffect } from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
-import { useMutation } from 'react-query'
-import axios from 'axios'
-import * as Sentry from '@sentry/react'
+import { useHistory } from 'react-router-dom'
 import 'styled-components/macro'
 import { Button, Spacer, textStyle, GU } from '@pokt-foundation/ui'
 import Onboarding from '../../components/Onboarding/Onboarding'
-import env from '../../environment'
-import { KNOWN_MUTATION_SUFFIXES } from '../../known-query-suffixes'
-import { sentryEnabled } from '../../sentry'
 import { useAuth0 } from '@auth0/auth0-react'
 
 export default function Validate() {
-  const { search } = useLocation()
   const history = useHistory()
-  const { loginWithRedirect } = useAuth0()
-
-  const token = new URLSearchParams(search).get('token')
-  const rawEmail = new URLSearchParams(search).get('email')
-  const email = decodeURIComponent(rawEmail)
-
-  const { isLoading, isSuccess, isError, mutate } = useMutation(
-    async function validate() {
-      try {
-        const path = `${env('BACKEND_URL')}/api/users/validate-user`
-
-        await axios.post(path, {
-          plainToken: token,
-          email,
-        })
-      } catch (err) {
-        if (sentryEnabled) {
-          Sentry.configureScope((scope) =>
-            scope.setTransactionName(
-              KNOWN_MUTATION_SUFFIXES.VALIDATE_USER_MUTATION
-            )
-          )
-          Sentry.captureException(err)
-        }
-
-        throw err
-      }
-    }
-  )
+  const { loginWithRedirect, user } = useAuth0()
 
   const goToLogin = useCallback(() => history.push('/login'), [history])
 
   useEffect(() => {
-    mutate()
-  }, [mutate])
+    if (user?.email_verified) {
+      goToLogin()
+    }
+  }, [user?.email_verified, goToLogin])
 
   return (
     <Onboarding>
-      <h2
+      <h1
         css={`
           ${textStyle('title1')}
           align-self: flex-start;
         `}
       >
-        {isLoading
-          ? 'Verifying your Account...'
-          : isSuccess
-          ? 'Verify your Account'
-          : 'Account Verification Failed'}
-      </h2>
-      <Spacer size={4 * GU} />
-      <div
+        Account Verification Pending
+      </h1>
+      <p
         css={`
-          position: relative;
-          z-index: 2;
-          width: 100%;
-          height: auto;
+          ${textStyle('body2')}
         `}
       >
-        {isLoading && (
-          <p
-            css={`
-              ${textStyle('body2')}
-            `}
-          >
-            Verifying...
-          </p>
-        )}
-        {isSuccess && (
-          <>
-            <p
-              css={`
-                ${textStyle('body2')}
-              `}
-            >
-              Now you can access the Portal to create endpoints for different
-              networks, and monitor your app infrastructure, all in one place.
-            </p>
-            <Spacer size={2 * GU} />
-            <p
-              css={`
-                ${textStyle('body2')}
-              `}
-            >
-              Welcome to Web3 done the right way!
-            </p>
-            <Spacer size={3 * GU} />
-            <Button
-              mode="primary"
-              onClick={() => loginWithRedirect()}
-              css={`
-                width: ${22.5 * GU}px;
-              `}
-            >
-              Log in
-            </Button>
-          </>
-        )}
-        {isError && (
-          <>
-            <p
-              css={`
-                ${textStyle('body2')}
-              `}
-            >
-              We sent you an email verification, please verify your email and
-              try again.
-            </p>
-            <Spacer size={4 * GU} />
-            <Button
-              mode="primary"
-              onClick={() => loginWithRedirect()}
-              css={`
-                width: ${22.5 * GU}px;
-              `}
-            >
-              Log in
-            </Button>
-          </>
-        )}
-      </div>
+        We sent you an email with a link to verify your account. Please complete
+        this action to proceed with login.
+      </p>
+      <Spacer size={4 * GU} />
+      <Button
+        mode="primary"
+        onClick={() => loginWithRedirect()}
+        css={`
+          width: ${22.5 * GU}px;
+        `}
+      >
+        Log in
+      </Button>
     </Onboarding>
   )
 }
